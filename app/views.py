b10692a6665e6f0923febb29633485414adb1228
Philip Guther <philip@guther.com>
2016-09-27 12:38:40 -0700

    if there are no results for a subdomain, "no results found" is now displayed

--- app/views.py
+++ app/views.py
@@ -7,19 +7,7 @@ from forms import SubdomainForm
 @app.route('/', methods=['GET', ])
 def index():
 
-    subdomain_dict = dict()
-
-    with open('app/urls.csv', 'rb') as csvfile:
-        csv_reader = csv.reader(csvfile)
-        for row in csv_reader:
-            if row[0] != 'Site':
-                site = row[0]
-                url = row[1]
-
-                if site in subdomain_dict:
-                    subdomain_dict[site].append(url)
-                else:
-                    subdomain_dict[site] = [url, ]
+    subdomain_dict = get_subdomain_dict()
 
     form = SubdomainForm()
     form.subdomain.choices = [(item, item) for item in sorted(subdomain_dict.keys())]
@@ -32,6 +20,21 @@ def index():
 
 @app.route('/results/<subdomain>')
 def results(subdomain):
+
+    subdomain_dict = get_subdomain_dict()
+
+    form = SubdomainForm()
+    form.subdomain.choices = [(item, item) for item in sorted(subdomain_dict.keys())]
+
+    if subdomain in subdomain_dict:
+        results = [(subdomain, url) for url in subdomain_dict[subdomain]]
+    else:
+        results = None
+
+    return render_template('index.html', form=form, results=results)
+
+
+def get_subdomain_dict():
     subdomain_dict = dict()
 
     with open('app/urls.csv', 'rb') as csvfile:
@@ -45,10 +48,4 @@ def results(subdomain):
                     subdomain_dict[site].append(url)
                 else:
                     subdomain_dict[site] = [url, ]
-
-    form = SubdomainForm()
-    form.subdomain.choices = [(item, item) for item in sorted(subdomain_dict.keys())]
-
-    results = [(subdomain, url) for url in subdomain_dict[subdomain]]
-
-    return render_template('index.html', form=form, results=results)
+    return subdomain_dict
