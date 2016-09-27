474a6629166b7e78fee1ae214c4821cfff7efa18
Philip Guther <philip@guther.com>
2016-09-27 12:30:15 -0700

    results now diplsay under their own url

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 import csv
 from forms import SubdomainForm
 
@@ -25,9 +25,30 @@ def index():
     form.subdomain.choices = [(item, item) for item in sorted(subdomain_dict.keys())]
 
     if 'subdomain' in request.args:
-        subdomain = request.args.get('subdomain')
-        results = [(subdomain, url) for url in subdomain_dict[subdomain]]
-
-        return render_template('index.html', form=form, results=results)
+        return redirect(url_for('results', subdomain=request.args.get('subdomain')))
 
     return render_template('index.html', form=form)
+
+
+@app.route('/results/<subdomain>')
+def results(subdomain):
+    subdomain_dict = dict()
+
+    with open('app/urls.csv', 'rb') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for row in csv_reader:
+            if row[0] != 'Site':
+                site = row[0]
+                url = row[1]
+
+                if site in subdomain_dict:
+                    subdomain_dict[site].append(url)
+                else:
+                    subdomain_dict[site] = [url, ]
+
+    form = SubdomainForm()
+    form.subdomain.choices = [(item, item) for item in sorted(subdomain_dict.keys())]
+
+    results = [(subdomain, url) for url in subdomain_dict[subdomain]]
+
+    return render_template('index.html', form=form, results=results)
